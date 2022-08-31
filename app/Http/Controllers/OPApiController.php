1eb2496ded467e54c81cadd4393f4be5f1355148
bodhisattwa das <bodhisattwadas@users.noreply.github.com>
2022-08-31 13:43:49 +0530

    SBI 12

--- app/Http/Controllers/OPApiController.php
+++ app/Http/Controllers/OPApiController.php
@@ -53,14 +53,15 @@ class OPApiController extends Controller
             
             $childArray['client_f_name'] = (array_key_exists('f2064//firstname',$element))?$element['f2064//firstname']:"";
             $childArray['client_l_name'] = (array_key_exists('f2064//lastname',$element))?$element['f2064//lastname']:"";
-            $childArray['client_state'] = (array_key_exists('f2064//statename',$element))?$element['f2064//statename']:"";
 
             $childArray['inspection_status'] = $element['f2009'];
 
             $childArray['access_details'] = $element['f2010'];
             $childArray['access_person_type'] = $element['f2105'];
-            $childArray['access_person'] = $element['f2106'];
 
+
+
+            $childArray['access_person'] = $element['f2106'];
             if(array_key_exists('f2106',$element) && $element['f2106'] != 0){
                 $personDetails =  Http::acceptJson()->get('https://api.ontraport.com/1/Contact',[
                     'Api-Appid' => '2_97024_DalLz1gO5',
@@ -73,41 +74,53 @@ class OPApiController extends Controller
                 $childArray['access_person_l_name'] = $personDetails['data']['lastname'];
                 $childArray['access_person_sms'] = $personDetails['data']['sms_number'];
                 $childArray['access_person_email'] = $personDetails['data']['email'];
-                // $childArray['client_state'] = $personDetails['data']['state'];
             }else{
                 $childArray['access_person_f_name'] = "";
                 $childArray['access_person_l_name'] = "";
                 $childArray['access_person_sms'] = "";
                 $childArray['access_person_email'] = "";
-                // $childArray['client_state'] = "";
+            }
+
+            if(array_key_exists('f2050',$element) && $element['f2050'] != 0){
+                $propertyDetails =  Http::acceptJson()->get('https://api.ontraport.com/1/Property',[
+                    'Api-Appid' => '2_97024_DalLz1gO5',
+                    'Api-Key' => 'xbEGYGGIBkMDn5H',
+                    'id' => $element['f2050'],
+                    'listFields' => 'f2687'
+                ]);
+                $childArray['suburb'] = $propertyDetails['data']['f2687'];
+                
+            }else{
+                $childArray['suburb'] = '';
             }
             $childArray['ontraport_link'] = "https://app.ontraport.com/#!/o_jobs10006/edit&id=".$element['id'];
             $childArray['date_of_inspection'] = $element['f2011'];
+
             array_push($parentArray,$childArray);
 
             /**
              * Database insert
              */
-            $opapi = new OPApi([
-                'job_id' => $childArray['job_id'],
-                'client_f_name' => $childArray['client_f_name'],
-                'client_l_name' => $childArray['client_l_name'],
-                'client_state' => $childArray['client_state'],
-                'inspection_status' => $childArray['inspection_status'],
-                'inspection_status_name' => $inspectionStatusArray[$childArray['inspection_status']],
-                'access_details' => $childArray['access_details'],
-                'access_person_type' => $childArray['access_person_type'],
-                'access_person_type_name' => $accessPersonTypeArray[$childArray['access_person_type']],
-                'access_person_f_name' => $childArray['access_person_f_name'],
-                'access_person_l_name' => $childArray['access_person_l_name'],
-                'access_person_sms' => $childArray['access_person_sms'],
-                'access_person_email' => $childArray['access_person_email'],
-                'ontraport_link' => $childArray['ontraport_link'],
-                'date_of_inspection' => $childArray['date_of_inspection']
-            ]);
-            if(OpApi::where('job_id',$childArray['job_id'])->get()->count() == 0) $opapi->save();
+            // $opapi = new OPApi([
+            //     'job_id' => $childArray['job_id'],
+            //     'client_f_name' => $childArray['client_f_name'],
+            //     'client_l_name' => $childArray['client_l_name'],
+            //     'client_state' => $childArray['client_state'],
+            //     'inspection_status' => $childArray['inspection_status'],
+            //     'inspection_status_name' => $inspectionStatusArray[$childArray['inspection_status']],
+            //     'access_details' => $childArray['access_details'],
+            //     'access_person_type' => $childArray['access_person_type'],
+            //     'access_person_type_name' => $accessPersonTypeArray[$childArray['access_person_type']],
+            //     'access_person_f_name' => $childArray['access_person_f_name'],
+            //     'access_person_l_name' => $childArray['access_person_l_name'],
+            //     'access_person_sms' => $childArray['access_person_sms'],
+            //     'access_person_email' => $childArray['access_person_email'],
+            //     'ontraport_link' => $childArray['ontraport_link'],
+            //     'date_of_inspection' => $childArray['date_of_inspection']
+            // ]);
+            //if(OpApi::where('job_id',$childArray['job_id'])->get()->count() == 0) $opapi->save();
         }
-        //print_r($parentArray);
+        print_r($parentArray);
     }
    
 }
--- app/Models/OPApi.php
+++ app/Models/OPApi.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class OPApi extends Model
 {
     use HasFactory;
-    protected $fillable = ['job_id','client_f_name','client_l_name','client_state','inspection_status',
+    protected $fillable = ['job_id','client_f_name','client_l_name','client_state','suburb_lat','suburb_long','inspection_status',
             'access_details','access_person_type','access_person_f_name','access_person_l_name',
         'access_person_sms','access_person_email','ontraport_link','date_of_inspection','inspection_status_name','access_person_type_name'];
 }
